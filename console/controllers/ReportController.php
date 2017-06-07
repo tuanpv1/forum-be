614c46584dfc01b86e2f8c71e9a825e2d8276f51
tuanpv <tuantrylook@gmail.com>
2017-06-07 09:04:18 -0700

    add

--- console/controllers/ReportController.php
+++ console/controllers/ReportController.php
@@ -34,6 +34,7 @@ use common\models\ReportSubscriberNumber;
 use common\models\ReportSubscriberService;
 use common\models\ReportTopics;
 use common\models\ReportTopup;
+use common\models\ReportUserPositive;
 use common\models\ReportUsers;
 use common\models\ReportVoucher;
 use common\models\Service;
@@ -95,17 +96,17 @@ class ReportController extends Controller
                 /** @var  $topic Topics */
                 foreach ($topics as $topic) {
                     $topic_id = $topic->topic_id;
-                    $like_count =PostsLikes::find()
-                        ->andWhere(['post_id'=>$topic_id])
+                    $like_count = PostsLikes::find()
+                        ->andWhere(['post_id' => $topic_id])
                         ->count();
                     $total_post = Posts::find()
                         ->innerJoin('phpbb_topics', 'phpbb_topics.topic_id = phpbb_posts.topic_id')
                         ->andWhere('phpbb_topics.topic_first_post_id <> phpbb_posts.post_id')
-                        ->andWhere(['topic_id'=>$topic_id])
+                        ->andWhere(['topic_id' => $topic_id])
                         ->count();
                     $view_count = $topic->topic_views;
                     $rate_count = 0;
-                    $rp_topic_add = ReportTopics::addNewRecord($to_day_date,$forum_id,$topic_id,$total_post,$like_count,$view_count,$rate_count);
+                    $rp_topic_add = ReportTopics::addNewRecord($to_day_date, $forum_id, $topic_id, $total_post, $like_count, $view_count, $rate_count);
 
                     if (!$rp_topic_add) {
                         echo '****** ERROR! Report voucher Daily Fail ******';
@@ -143,17 +144,17 @@ class ReportController extends Controller
 
             echo "Deleted report game daily date:" . date("d-m-Y H:i:s", $beginPreDay) . ' timestamp:' . $beginPreDay;
             ReportUsers::deleteAll(['between', 'report_date', $beginPreDay, $endPreDay]);
-            $group = [Groups::GROUP_NEWLY_REGISTEREDLY,Groups::GROUP_REGISTERED];
+            $group = [Groups::GROUP_NEWLY_REGISTEREDLY, Groups::GROUP_REGISTERED];
             $total_user = Users::find()
                 ->andWhere(['IN', 'group_id', $group])
-                ->andWhere(['user_inactive_reason'=>Users::USER_TYPE_ACTIVE])
+                ->andWhere(['user_inactive_reason' => Users::USER_TYPE_ACTIVE])
                 ->count();
             $user_ban = Banlist::find()->andWhere(['between', 'ban_start', $beginPreDay, $endPreDay])->count();
             $user_new = Users::find()
-                ->andWhere(['user_inactive_reason'=>Users::USER_TYPE_INACTIVE])
+                ->andWhere(['user_inactive_reason' => Users::USER_TYPE_INACTIVE])
                 ->andWhere(['IN', 'group_id', $group])
                 ->count();
-            $addReportUser = ReportUsers::addNewRecord($beginPreDay,$total_user,$user_ban,$user_new);
+            $addReportUser = ReportUsers::addNewRecord($beginPreDay, $total_user, $user_ban, $user_new);
             if (!$addReportUser) {
                 echo '****** ERROR! Report voucher Daily Fail ******';
                 $transaction->rollBack();
@@ -188,34 +189,111 @@ class ReportController extends Controller
 
             echo "Deleted report game daily date:" . date("d-m-Y H:i:s", $beginPreDay) . ' timestamp:' . $beginPreDay;
             LikeCommentUser::deleteAll(['between', 'report_date', $beginPreDay, $endPreDay]);
-            $group = [Groups::GROUP_NEWLY_REGISTEREDLY,Groups::GROUP_REGISTERED];
+            $group = [Groups::GROUP_NEWLY_REGISTEREDLY, Groups::GROUP_REGISTERED];
             $users = Users::find()
                 ->andWhere(['IN', 'group_id', $group])
-                ->andWhere(['user_inactive_reason'=>Users::USER_TYPE_ACTIVE])
+                ->andWhere(['user_inactive_reason' => Users::USER_TYPE_ACTIVE])
                 ->all();
-            if(!$users){
+            if (!$users) {
                 $transaction->rollBack();
-                echo  '****** LỖI! Khong co User ******';
+                echo '****** LỖI! Khong co User ******';
             }
-            foreach ($users as $user){
+            foreach ($users as $user) {
                 /** @var Users $user */
                 $user_id = $user->user_id;
                 $like_count = PostsLikes::find()
-                    ->andWhere(['user_id'=>$user_id])
+                    ->andWhere(['user_id' => $user_id])
                     ->andWhere(['between', 'timestamp', $beginPreDay, $endPreDay])
-                    ->andWhere(['type'=>'post'])
+                    ->andWhere(['type' => 'post'])
                     ->count();
                 $comment_true_count = Posts::find()
                     ->andWhere(['between', 'post_time', $beginPreDay, $endPreDay])
-                    ->andWhere(['post_status_display'=>Posts::STATUS_ANSWER_RIGHT])
-                    ->andWhere(['poster_id'=>$user_id])
+                    ->andWhere(['post_status_display' => Posts::STATUS_ANSWER_RIGHT])
+                    ->andWhere(['poster_id' => $user_id])
                     ->count();
                 $comment_false_count = Posts::find()
                     ->andWhere(['between', 'post_time', $beginPreDay, $endPreDay])
-                    ->andWhere(['post_status_display'=>Posts::STATUS_ANSWER_WRONG])
-                    ->andWhere(['poster_id'=>$user_id])
+                    ->andWhere(['post_status_display' => Posts::STATUS_ANSWER_WRONG])
+                    ->andWhere(['poster_id' => $user_id])
                     ->count();
-                $addReportUser = LikeCommentUser::addNewRecord($beginPreDay,$user_id,$like_count,$comment_true_count, $comment_false_count);
+                $addReportUser = LikeCommentUser::addNewRecord($beginPreDay, $user_id, $like_count, $comment_true_count, $comment_false_count);
+                if (!$addReportUser) {
+                    echo '****** ERROR! Report voucher Daily Fail ******';
+                    $transaction->rollBack();
+                }
+                $transaction->commit();
+                echo '****** Chay bao cao hoan thanh! ******';
+            }
+            $transaction->commit();
+            echo '****** Chay bao cao hoan thanh! ******';
+
+        } catch (Exception $e) {
+            $transaction->rollBack();
+            echo '****** LOI! Chay bao cao khong thanh cong: ' . $e->getMessage() . '******';
+        }
+    }
+
+    public function actionReportUserPositive($start_day = '')
+    {
+        $config_sysyem = [];
+        if(!$config_sysyem){
+            print(" Lỗi! Chưa cấu hình tham số  \n");
+            exit();
+        }
+        $number_like_positive = $config_sysyem->number_like_positive;
+        $number_answer_positve = $config_sysyem->number_answer_positvie;
+        if(!$number_answer_positve && !$number_like_positive){
+            print(" Lỗi! Chưa cấu hình tham số  \n");
+            exit();
+        }
+        $transaction = Yii::$app->db->beginTransaction();
+        try {
+            print("Bao cao bat dau chay \n");
+
+            if ($start_day != '') {
+                $beginPreDay = strtotime(DateTime::createFromFormat("dmY", $start_day)->setTime(0, 0)->format('Y-m-d H:i:s'));
+                $endPreDay = strtotime(DateTime::createFromFormat("dmY", $start_day)->setTime(23, 59, 59)->format('Y-m-d H:i:s'));
+                $to_day_date = DateTime::createFromFormat("dmY", $start_day)->setTime(0, 0)->format('Y-m-d H:i:s');
+            } else {
+                $beginPreDay = strtotime("midnight", time());
+                $endPreDay = strtotime("tomorrow", $beginPreDay) - 1;
+                $to_day_date = (new DateTime('now'))->setTime(0, 0)->format('Y-m-d H:i:s');
+            }
+
+            print("Thoi gian bat dau: $beginPreDay : Thoi gian ket thuc: $endPreDay ");
+            print("Chuyen sang ngay: $to_day_date \n");
+
+            echo "Deleted report game daily date:" . date("d-m-Y H:i:s", $beginPreDay) . ' timestamp:' . $beginPreDay;
+            ReportUserPositive::deleteAll(['between', 'report_date', $beginPreDay, $endPreDay]);
+            $group = [Groups::GROUP_NEWLY_REGISTEREDLY, Groups::GROUP_REGISTERED];
+            $users_id = LikeCommentUser::find()
+                ->select('user_id')
+                ->andWhere(['>=', 'like_count', $number_like_positive])
+                ->andWhere(['between', 'report_date', $beginPreDay, $endPreDay])
+                ->all();
+            if (!$users) {
+                $transaction->rollBack();
+                echo '****** LỖI! Khong co User ******';
+            }
+            foreach ($users as $user) {
+                /** @var Users $user */
+                $user_id = $user->user_id;
+                $like_count = PostsLikes::find()
+                    ->andWhere(['user_id' => $user_id])
+                    ->andWhere(['between', 'timestamp', $beginPreDay, $endPreDay])
+                    ->andWhere(['type' => 'post'])
+                    ->count();
+                $comment_true_count = Posts::find()
+                    ->andWhere(['between', 'post_time', $beginPreDay, $endPreDay])
+                    ->andWhere(['post_status_display' => Posts::STATUS_ANSWER_RIGHT])
+                    ->andWhere(['poster_id' => $user_id])
+                    ->count();
+                $comment_false_count = Posts::find()
+                    ->andWhere(['between', 'post_time', $beginPreDay, $endPreDay])
+                    ->andWhere(['post_status_display' => Posts::STATUS_ANSWER_WRONG])
+                    ->andWhere(['poster_id' => $user_id])
+                    ->count();
+                $addReportUser = LikeCommentUser::addNewRecord($beginPreDay, $user_id, $like_count, $comment_true_count, $comment_false_count);
                 if (!$addReportUser) {
                     echo '****** ERROR! Report voucher Daily Fail ******';
                     $transaction->rollBack();
--- console/migrations/m170606_225608_create_phpbb_report_user_positive_table.php
+++ console/migrations/m170606_225608_create_phpbb_report_user_positive_table.php
@@ -15,8 +15,8 @@ class m170606_225608_create_phpbb_report_user_positive_table extends Migration
         $this->createTable('phpbb_report_user_positive', [
             'id' => $this->primaryKey(),
             'date_report' => $this->integer(),
-            'user_positive_id' => $this->integer(),
-            'user_negative_id' => $this->integer(),
+            'user_positive_id' => $this->text(),
+            'user_negative_id' => $this->text(),
         ]);
     }
 
